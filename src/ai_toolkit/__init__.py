37d8ae9de21dbab6a4df64cb8de1892ae1d12519
Keaton Conrad <keaton.conrad@gmail.com>
2024-01-16 22:48:46 -0600

    Fixes imports

--- src/ai_toolkit/__init__.py
+++ src/ai_toolkit/__init__.py
@@ -1,5 +1,5 @@
-from comet import CometMLClient
-from processor import DataProcessor
-from utils import set_seeds, set_global_determinism
+from .comet import CometMLClient
+from .processor import DataProcessor
+from .utils import set_seeds, set_global_determinism
 
 __all__ = ["CometMLClient", "DataProcessor", "set_seeds", "set_global_determinism"]
